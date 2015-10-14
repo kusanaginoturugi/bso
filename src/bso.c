97a1eac0cb4ed3c9df88c51e8c6ef53082ad4ebc
Takuro Onoue <onoue@showway.biz>
2015-10-14 18:08:46 +0900

    Add elapsed time.

--- src/bso.c
+++ src/bso.c
@@ -5,6 +5,7 @@ static Window *window;
 static TextLayer *ball_layer;
 static TextLayer *strike_layer;
 static TextLayer *out_layer;
+static TextLayer *timer_layer;
 
 static int ball;
 static int strike;
@@ -14,6 +15,8 @@ static int history_count = 0;
 #define MAX_TYPE    3
 #define MAX_HISTORY 100
 static int history[3][MAX_HISTORY];
+
+static int s_uptime = 0;
 
 /* プロトタイプ宣言 */
 static void show_ball();
@@ -27,6 +30,13 @@ static void push(int type, int count)
 {
   history_count++;
   if (history_count == 1) {
+    if (s_uptime == 0) {
+      s_uptime = 1;
+      ball = 0;
+      strike = 0;
+      out = 0;
+      text_layer_set_text(timer_layer, "Time: 0h:0m");      
+    }
     for (int i = 0; i < MAX_TYPE; i++)
     {
         history[i][0] = history[i][0];
@@ -238,6 +248,25 @@ static void click_config_provider(void *context) {
   /* window_long_click_subscribe(BUTTON_ID_DOWN,   DELAY_MS, NULL, down_long_click_handler); */
 }
 
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  if (s_uptime > 0) {
+    // Use a long-lived buffer
+    static char s_uptime_buffer[32];
+
+    // Get time since launch
+    //int seconds = s_uptime % 60;
+    int minutes = (s_uptime % 3600) / 60;
+    int hours = s_uptime / 3600;
+
+    // Update the TextLayer
+    snprintf(s_uptime_buffer, sizeof(s_uptime_buffer), "Time: %dh %dm", hours, minutes);
+    text_layer_set_text(timer_layer, s_uptime_buffer);
+
+    // Increment s_uptime
+    s_uptime += 60;
+  }
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -248,17 +277,23 @@ static void window_load(Window *window) {
   text_layer_set_font(ball_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   layer_add_child(window_layer, text_layer_get_layer(ball_layer));
 
-  strike_layer = text_layer_create((GRect) { .origin = { 0, 55 }, .size = { bounds.size.w, 40 } });
+  strike_layer = text_layer_create((GRect) { .origin = { 0, 45 }, .size = { bounds.size.w, 40 } });
   text_layer_set_text(strike_layer, "B S O");
   text_layer_set_text_alignment(strike_layer, GTextAlignmentCenter);
   text_layer_set_font(strike_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   layer_add_child(window_layer, text_layer_get_layer(strike_layer));
 
-  out_layer = text_layer_create((GRect) { .origin = { 0, 105 }, .size = { bounds.size.w, 40 } });
+  out_layer = text_layer_create((GRect) { .origin = { 0, 85 }, .size = { bounds.size.w, 40 } });
   text_layer_set_text(out_layer, "");
   text_layer_set_text_alignment(out_layer, GTextAlignmentCenter);
   text_layer_set_font(out_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
   layer_add_child(window_layer, text_layer_get_layer(out_layer));
+
+  timer_layer = text_layer_create((GRect) { .origin = { 0, 125 }, .size = { bounds.size.w, 30 } });
+  text_layer_set_text(timer_layer, "Play Ball >");
+  text_layer_set_text_alignment(timer_layer, GTextAlignmentRight);
+  text_layer_set_font(timer_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(timer_layer));
 
   ball       = 0;
   strike     = 0;
@@ -269,6 +304,7 @@ static void window_unload(Window *window) {
   text_layer_destroy(ball_layer);
   text_layer_destroy(strike_layer);
   text_layer_destroy(out_layer);
+  text_layer_destroy(timer_layer);
 }
 
 static void init(void) {
@@ -280,6 +316,9 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(window, animated);
+
+  // Subscribe to TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(void) {
